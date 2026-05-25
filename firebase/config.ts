42df149b20f4a298eb9d4e9c58acd14799903731
Ananthu <ananthugr2018@gmail.com>
2026-05-25 05:35:06 +0530

    Added environment variables

--- firebase/config.ts
+++ firebase/config.ts
@@ -12,13 +12,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAAR-EJT1HoH3z67hzDhUejjcWTPbibUnY",
-  authDomain: "psc-companion-c414a.firebaseapp.com",
-  projectId: "psc-companion-c414a",
-  storageBucket: "psc-companion-c414a.firebasestorage.app",
-  messagingSenderId: "952920113239",
-  appId: "1:952920113239:web:9678e0d7a4c8f82cad2312",
-  measurementId: "G-ZT4M64FR9P"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  
 };
 
 // Initialize Firebase
